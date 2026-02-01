087f5d8f76b1866439c2bc908908b540f0950e9c
Rohit patil <136801508+Rohitpatil34@users.noreply.github.com>
2026-02-01 22:19:52 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Certifications</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="assets/docs/rohit newlyupdated resume.pdf" target="_blank">Resume</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="assets/docs/RohitPatil_Resume (1).pdf" target="_blank">Resume</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
    </li>
@@ -479,4 +479,5 @@ $(function submitAnimation() {
     }
   });
 });
+
 
